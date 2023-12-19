97e39e2585874dd79c9b02204f0fe9e1f6e91472
nuridze <113994431+nuridze@users.noreply.github.com>
2023-12-19 15:56:49 +0100

    1

--- js/task-1.js
+++ js/task-1.js
@@ -39,4 +39,5 @@ console.log(
       balance: 2764,
     },
   ])
-); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+); 
+// ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
--- js/task-3.js
+++ js/task-3.js
@@ -43,6 +43,7 @@ console.log(
     },
   ])
 );
+
 // [
 //   {
 //     name: "Ross Vazquez",
